1b1a885765700ef2394d7fefb734bef44b399d66
Sérgio Oliveira <sergiowallace11@hotmail.com>
2024-12-10 11:29:29 +0000

    project started. first commit.

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
+      "2xl": "1600px",
+    },
+    fontFamily: {
+      primary: ["JetBrains", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#0a0a0a",
+        accent: {
+          DEFAULT: "#0086b0",
+          hover: "#01579b",
+        },
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+      },
+    },
+  },
+};
