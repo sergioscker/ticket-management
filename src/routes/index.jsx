4ba1188c669bcf7e9cc6912d6dbddcc85ee1a432
Sérgio Oliveira <sergiowallace11@hotmail.com>
2024-12-10 18:06:28 +0000

    creation of the HomePage.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,10 @@
-import { Home } from '@/pages/Home';
+import { HomePage } from '@/pages/Home';
 import { Login } from '@/pages/Login';
 import { CreateTicket } from '@/pages/Create-tickets';
 import { User } from '@/pages/User';
 
 export const routes = [
-  { path: '/', element: <Home /> },
+  { path: '/', element: <HomePage /> },
   { path: '/login', element: <Login /> },
   { path: '/create-ticket', element: <CreateTicket /> },
   { path: '/user', element: <User /> },
